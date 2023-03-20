7a781fec2f90e334d1b665c8cea427c67950457f
Sarthak Shrivastava <sarthakrishu@gmail.com>
2023-03-20 20:03:38 +0530

    Initial Commit

---
+++ Weather Intro/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Weather Intro
+//
+//  Created by Sarthak Shrivastava on 20/03/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Weather Intro/Weather_IntroApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_IntroApp.swift
+//  Weather Intro
+//
+//  Created by Sarthak Shrivastava on 20/03/23.
+//
+
+import SwiftUI
+
+@main
+struct Weather_IntroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
